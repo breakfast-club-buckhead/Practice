78349adb34efa0ca3bb6516c98776f9c92f9266f
nerajno <nerajno@gmail.com>
2021-07-19 19:18:49 -0400

    almost done || got help from Alex

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function addDish(e){
     const dish = { dishText, 
                     done: false
                 };
-
     dishes.push(dish);
     populateDish(dishes, savedDishes);
     localStorage.setItem('dishes', JSON.stringify(dishes));
@@ -16,33 +15,48 @@ function addDish(e){
 }
 
 function deleteDish(e){
-
+  e.preventDefault();
+ console.log("0");
+  //find index of dish && splice(index, 1) || ensure index != -1
+  // set items dishes in local storage
+  // onClick of pot, strike through and change style to italic && bold
+  // remove from localstorage
 }
 
 function populateDish(writtenDishes = [], displayedDishes){
     displayedDishes.innerHTML = writtenDishes.map((dish, i) => {
-        console.log(dish,dish.dishText.value, i);
         return `
           <li>
             <input type="checkbox" data-index=${i} id="item${i}" ${dish.done ? 'checked' : ''} />
             <label for="item${i}">${dish.dishText}</label>
+            <button data-index=${i} class="delete">🗑️</button>
           </li>
         `;
       }).join('');
 }
 
-function toggleDone(e){
+function toggleDone(e){ 
     if(!e.target.matches('input')) return;
+    console.log("1");
     const el = e.target;
     const index = el.dataset.index;
     dishes[index].done = !dishes[index].done;
     localStorage.setItem('dishes', JSON.stringify(dishes));
     populateDish(dishes, savedDishes);
+
   }
 
+function clickHandler(e){
+
+  if(e.target.matches('label')){
+    return toggleDone.call(this,e);
+  }else if(e.target.matches('button')){
+   return deleteDish.call(this, e);
+  }
+}
 
 
-  addDishes.addEventListener('submit', addDish);
-  savedDishes.addEventListener('click', toggleDone);
-console.log(localStorage.getItem("dishes"));
-  populateDish(dishes, savedDishes);
+addDishes.addEventListener('submit', addDish);
+savedDishes.addEventListener('click', clickHandler);
+// console.log(localStorage.getItem("dishes"));
+populateDish(dishes, savedDishes);
